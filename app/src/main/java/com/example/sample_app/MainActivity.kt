c712535fa97584d5531c33c0c960ee957125eaa6
iseruuuuu <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>
2023-05-05 16:26:03 +0900

    add: create file

---
+++ app/src/main/java/com/example/sample_app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.sample_app
+
+//import FirstScreen
+//import SecondScreen
+//import ThirdScreen
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "first") {
+                composable("first") {
+                    FirstScreen(navController)
+                }
+                composable("second") {
+                    SecondScreen(navController)
+                }
+                composable("third") {
+                    ThirdScreen(navController)
+                }
+            }
+        }
+    }
+}
